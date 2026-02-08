ca02d574e07cefdbee5f504e2b8087d9b9349cee
KJeyachandran <kjeyachandran@hotmail.com>
2026-02-08 17:44:57 +0000

    Tidy up OrderTests class.

--- src/TradeCapture.Tests/OrderTests.cs
+++ src/TradeCapture.Tests/OrderTests.cs
@@ -1,7 +1,4 @@
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using TradeCapture.Foundation.Events;
 using TradeCapture.Foundation.Orders;
 using Xunit;
